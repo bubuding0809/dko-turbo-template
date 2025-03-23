a05ed733042be9bbd1db0d8ceddf3b388968d4e6
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-03-23 19:21:13 +0800

    fix(lambda): update server log to include full URL with port

--- apps/lambda/index.dev.ts
+++ apps/lambda/index.dev.ts
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log("NODE_ENV:", process.env.NODE_ENV);
-  console.log(`🚀 Server running on ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
