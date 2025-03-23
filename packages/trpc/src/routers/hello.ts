76d227bfaca46f482dcccc9eb6324d5d510913fe
dingruoqian99 <ding.ruoqian@synapxe.sg>
2025-03-23 18:51:47 +0800

    feat(trpc): add Zod for input validation and implement user query

--- packages/trpc/src/routers/hello.ts
+++ packages/trpc/src/routers/hello.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { publicProcedure } from "../trpc.js";
 
 export const helloRouter = {
@@ -6,6 +7,14 @@ export const helloRouter = {
       greeting: `Hello World!`,
     };
   }),
+  user: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input, ctx }) => {
+      const user = await ctx.db.user.findUnique({
+        where: { id: input.id },
+      });
+      return user;
+    }),
   users: publicProcedure.query(async ({ ctx }) => {
     const users = await ctx.db.user.findMany();
     return users;
